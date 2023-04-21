50187c2aaa08978ed183e74d5625e96c09416493
SahilNevalkar <sahil.nevalkar.external@smartdings.com>
2023-04-21 09:58:02 +0530

    update: urls and route

--- src/urls.ts
+++ src/urls.ts
@@ -2,12 +2,16 @@ const root = "/";
 const selectworkers = "/selectworkers";
 const home = "/home";
 const orderdetails = "/orderdetails";
+const memberdetails = "/memberdetails";
+const confirmorderdetails = "/confirmorderdetails";
 
 const urls = {
   root,
   default: home,
   selectworkers,
   orderdetails,
+  memberdetails,
+  confirmorderdetails,
 };
 
 export default urls;
