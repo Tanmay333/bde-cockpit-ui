70aa52796619b4a08b89b99a1a7d7cadc6fdfd8a
Arjun <arjun.jadhav.external@smartdings.com>
2023-04-24 15:23:15 +0530

    add:(redux store)

--- src/components/orderInfoCard/OrderInfoCard.tsx
+++ src/components/orderInfoCard/OrderInfoCard.tsx
@@ -4,15 +4,32 @@ import {
   IonCardHeader,
   IonCardSubtitle,
   IonCardTitle,
-} from "@ionic/react";
-import React from "react";
-import styles from "./OrderInforCard.module.scss";
+} from '@ionic/react';
+import React, { useEffect } from 'react';
+import styles from './OrderInforCard.module.scss';
+import { useAppDispatch, useAppSelector } from '../../store/utils/hooks';
+import { getTestData } from '../../store/slices/OrderInfoSlice';
 
 const OrderInfoCard: React.FC = () => {
+  const state = useAppSelector((state) => state.testslice);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const promise = dispatch(getTestData());
+
+    return () => {
+      promise.abort();
+    };
+  }, [dispatch]);
+
+  if (state.data === null) {
+    return null;
+  }
+
   return (
     <IonCard className={styles.orderInfoCard}>
       <IonCardHeader>
-        <IonCardTitle>Order: 382993844</IonCardTitle>
+        <IonCardTitle>{state.data.name}</IonCardTitle>
         <IonCardSubtitle>Machine on</IonCardSubtitle>
       </IonCardHeader>
 
--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,25 +1,25 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import imagesReducer from "./imagesSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import { TEST_SLICE_KEY, testDataReducer } from './slices/OrderInfoSlice';
 
-export function createStore() {
-  const store = configureStore({
-    reducer: {
-      images: imagesReducer,
-    },
-  });
+const reducers = {
+  [TEST_SLICE_KEY]: testDataReducer,
+};
 
-  return store;
+export const store = configureStore({ reducer: reducers });
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export interface AppThunkConfig {
+  dispatch: AppDispatch;
+  state: RootState;
 }
 
-const appStore: ReturnType<typeof createStore> = createStore();
-
-export default appStore;
-
-export type AppDispatch = typeof appStore.dispatch;
-export type RootState = ReturnType<typeof appStore.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+// This is going to be used to provide a store for integration tests.
+export const createAppStore = (preloadedState?: Partial<RootState>) => {
+  return configureStore({
+    preloadedState,
+    reducer: reducers,
+  });
+};
