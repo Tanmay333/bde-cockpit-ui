473af11c3a17fde2e03d80f073a682a3ede3b64c
SahilNevalkar <sahil.nevalkar.external@smartdings.com>
2023-04-27 11:38:23 +0530

    add: header component

--- src/components/donwtimeType/DowntimeType.tsx
+++ src/components/donwtimeType/DowntimeType.tsx
@@ -1,27 +1,33 @@
-import { IonButton, IonRow } from '@ionic/react';
+import { IonButton, IonContent, IonPage, IonRow } from '@ionic/react';
 import React from 'react';
 import styles from './DowntimeType.module.scss';
+import Header from '../common/header/Header';
 
 const DowntimeType: React.FC = () => {
   return (
-    <>
-      <div className={styles.title}>
-        <p>Downtime</p>
-      </div>
+    <IonPage>
+      <Header />
+      <IonContent>
+        <div className={styles.title}>
+          <p>Downtime</p>
+        </div>
 
-      <div>
-        <IonRow>
-          <IonButton className={styles.button}>Machine issue</IonButton>
-          <IonButton className={styles.button}>Lunch Break</IonButton>
-          <IonButton className={styles.button}>Sick leave</IonButton>
-          <IonButton className={styles.button}>Team meeting </IonButton>
-          <IonButton className={styles.button}>Urgent call</IonButton>
-        </IonRow>
-      </div>
-      <div className={styles.endBtn}>
-        <IonButton className={styles.end}>End Production</IonButton>
-      </div>
-    </>
+        <div>
+          <IonRow>
+            <IonButton className={styles.button}>Machine issue</IonButton>
+            <IonButton className={styles.button}>Lunch Break</IonButton>
+            <IonButton className={styles.button}>Sick leave</IonButton>
+            <IonButton className={styles.button}>Team meeting </IonButton>
+            <IonButton className={styles.button}>Urgent call</IonButton>
+          </IonRow>
+        </div>
+        <div className={styles.endBtn}>
+          <IonButton className={styles.end} href="/">
+            End Production
+          </IonButton>
+        </div>
+      </IonContent>
+    </IonPage>
   );
 };
 
