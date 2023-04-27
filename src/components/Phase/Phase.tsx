f3694e6a680a2235c6a783775a0d6348323e6021
SahilNevalkar <sahil.nevalkar.external@smartdings.com>
2023-04-27 11:42:22 +0530

    update: page route and UI

--- src/components/Phase/Phase.tsx
+++ src/components/Phase/Phase.tsx
@@ -1,48 +1,60 @@
-import { IonGrid, IonRow, IonCol } from '@ionic/react';
-import React from 'react';
-
-// interface Props {
-//   isSelected: boolean;
-// }
-// function clickablediv() {
-//   ".aaa".click(function () {
-//     this.addClass("selected");
-//   });
-// };
-// function clickablediv() {
-//   document.getElementById("aaa")?.style.background = "green";
-
-// }
+import { IonGrid, IonRow, IonCol, IonModal } from '@ionic/react';
+import React, { useCallback, useRef, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Phase: React.FC = () => {
+  const [downTime, setDownType] = useState(false);
+
+  const modal = useRef<HTMLIonModalElement>(null);
+  const history = useHistory();
+
+  const onDownTimeComplete = useCallback(() => {
+    history.push('/downtimetype');
+    setDownType(false);
+  }, [history]);
+
+  const [bgColor, setBgColor] = useState('#E0E0E0');
+
+  const onClick = useCallback(() => {
+    setBgColor('#2799D1');
+  }, [bgColor]);
+
+  const handleClick = useCallback(() => {
+    setBgColor('#2799D1');
+    history.push('/SelectWorkers');
+  }, [bgColor]);
+
   return (
     <IonGrid>
       <IonCol>
         <IonGrid>
           <IonRow>
             <div
-              //id="aaa"
-              //onClick={clickablediv}
-
+              onClick={onClick}
+              //onClick={handleClick}
               style={{
                 height: '39px',
                 width: '105px',
-                background: '#E0E0E0',
+                //background: '#E0E0E0',
+                backgroundColor: bgColor,
                 borderRadius: '5px',
                 margin: '3px',
               }}
             ></div>
-            <a href="/SelectWorkers">
-              <div
-                style={{
-                  height: '39px',
-                  width: '101px',
-                  background: '#E0E0E0',
-                  borderRadius: '5px',
-                  margin: '3px',
-                }}
-              ></div>
-            </a>
+            {/* <a href="/SelectWorkers"> */}
+            <div
+              onClick={handleClick}
+              style={{
+                height: '39px',
+                width: '101px',
+                //background: '#E0E0E0',
+                backgroundColor: bgColor,
+                borderRadius: '5px',
+                margin: '3px',
+              }}
+            ></div>
+            {/* </a> */}
+            {/* <a href="/DowntimeType"> */}
             <div
               style={{
                 height: '39px',
@@ -51,7 +63,16 @@ const Phase: React.FC = () => {
                 borderRadius: '5px',
                 margin: '3px',
               }}
+              onClick={onDownTimeComplete}
             ></div>
+            <IonModal
+              style={{
+                '--width': '100%',
+                '--height': '100%',
+              }}
+              ref={modal}
+              isOpen={downTime}
+            ></IonModal>
             <div
               style={{
                 height: '39px',
