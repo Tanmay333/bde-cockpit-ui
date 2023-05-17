0d73c43b7d8f9bc132a196c9d538204a0deac0d6
SahilNevalkar <sahil.nevalkar.external@smartdings.com>
2023-05-17 11:17:20 +0530

    add(PhaseDetails): message to the API from the app

--- src/components/phaseDetails/PhaseDetails.tsx
+++ src/components/phaseDetails/PhaseDetails.tsx
@@ -1,8 +1,33 @@
 import CardContainer from '../common/cardContainer/CardContainer';
 import { IonCardContent, IonGrid, IonButton } from '@ionic/react';
 import styles from './PhaseDetails.module.scss';
+import { useCallback } from 'react';
+import { useAppDispatch } from '../../store/utils/hooks';
+import { getMachineDetails } from '../../store/slices/machineDetailsSlice';
+import { useHistory } from 'react-router';
 
 const PhaseDetails: React.FC = () => {
+  const history = useHistory();
+  const dispatch = useAppDispatch();
+
+  const onEndUnmounting = useCallback(() => {
+    dispatch(
+      getMachineDetails({
+        action: 'setEndOfUnmounting',
+        jobId: '782e0622-c9d8-4f5d-a026-d51ef99f2c08',
+      }),
+    );
+  }, [history]);
+
+  const onEndCleaning = useCallback(() => {
+    dispatch(
+      getMachineDetails({
+        action: 'setEndOfCleaning',
+        jobId: '782e0622-c9d8-4f5d-a026-d51ef99f2c08',
+      }),
+    );
+  }, [history]);
+
   return (
     <>
       <CardContainer title="Phase details" position={'start'}>
@@ -30,6 +55,18 @@ const PhaseDetails: React.FC = () => {
               color={'danger'}
             >
               Mounting
+            </IonButton>
+            <IonButton
+              onClick={onEndUnmounting}
+              type="submit"
+              fill="solid"
+              style={{
+                width: '210px',
+                height: '50px',
+              }}
+              color={'danger'}
+            >
+              End UnMounting
             </IonButton>
           </div>
         </IonGrid>
