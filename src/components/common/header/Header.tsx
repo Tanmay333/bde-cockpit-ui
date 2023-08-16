335fd67d23cc29b4203c6cea8fa68c4e7a58e1e5
SahilNevalkar <sahil.nevalkar.external@smartdings.com>
2023-08-16 12:37:44 +0530

    remove: logos

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,15 +1,11 @@
-import styles from "./Header.module.scss";
-import LohnpackLogo from "../../../static/assets/images/LohnpackLogo.svg";
-import PSITechnicsLogo from "../../../static/assets/images/PSILogo.svg";
-import SmartdingsLogo from "../../../static/assets/images/SmartdingsLogo.svg";
+import styles from './Header.module.scss';
+import LohnpackLogo from '../../../static/assets/images/LohnpackLogo.svg';
 
 const Header = () => {
   return (
     <div className={styles.container}>
       <div className={styles.logoContainer}>
-        <img src={LohnpackLogo} alt={"lohnpack logo"} />
-        <img src={PSITechnicsLogo} alt={"psi logo"} />
-        <img src={SmartdingsLogo} alt={"smartdings logo"} />
+        <img src={LohnpackLogo} alt={'lohnpack logo'} />
       </div>
     </div>
   );
