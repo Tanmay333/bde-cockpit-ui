e87835e6e6101e5611f09e2c07c5c7b7e3902c23
Pranav <pranav.nanche@smartdings.com>
2023-08-22 15:09:55 +0530

    update: review points

--- src/test/test.utils.tsx
+++ src/test/test.utils.tsx
@@ -1,47 +1,46 @@
-import { ReactElement, FC } from "react";
-import { Provider } from "react-redux";
-import { render as rtlRender, RenderOptions, screen, waitForElementToBeRemoved } from '@testing-library/react'
-import { createAppStore, RootState } from "../store/configureStore";
-import { SupportedLanguage } from "../assets/translations/common";
-
+import { ReactElement, FC } from 'react';
+import { Provider } from 'react-redux';
+import {
+  render as rtlRender,
+  RenderOptions,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import { createAppStore, RootState } from '../store/configureStore';
+import { SupportedLanguage } from '../assets/translations/common';
 
 interface RenderOptionsWithState extends RenderOptions {
-    initialState?: Partial<RootState>;
+  initialState?: Partial<RootState>;
 }
 
-
 const render = (
-    ui: ReactElement,
-    { initialState, ...renderOptions }: RenderOptionsWithState = {},
+  ui: ReactElement,
+  { initialState, ...renderOptions }: RenderOptionsWithState = {},
 ) => {
-    const store = createAppStore({
-        ...testTranslationState,
-        ...initialState,
-    });
-    const Wrapper: FC<{ children: ReactElement }> = ({ children }) => (
-        <Provider store={store}>{children}</Provider>
-    );
-
-    return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+  const store = createAppStore({
+    ...testTranslationState,
+    ...initialState,
+  });
+  const Wrapper: FC<{ children: ReactElement }> = ({ children }) => (
+    <Provider store={store}>{children}</Provider>
+  );
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
 };
+
 const testTranslationState: Partial<RootState> = {
-    translation: {
-
-        language: SupportedLanguage.ENGLISH,
-        locale: SupportedLanguage.ENGLISH,
-
-    },
+  translation: {
+    language: SupportedLanguage.ENGLISH,
+    locale: SupportedLanguage.ENGLISH,
+  },
 };
 
 const delay = (ms: number) =>
-    new Promise((resolve) => window.setTimeout(resolve, ms));
+  new Promise((resolve) => window.setTimeout(resolve, ms));
 
 const waitForLoading = async (testId: string): Promise<void> => {
-    await screen.findByTestId(testId);
-    await waitForElementToBeRemoved(() => screen.queryByTestId(testId));
+  await screen.findByTestId(testId);
+  await waitForElementToBeRemoved(() => screen.queryByTestId(testId));
 };
 
 export * from '@testing-library/react';
-export {
-    render, delay, waitForLoading
-}
+export { render, delay, waitForLoading };
